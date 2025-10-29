7aa928d6b36fc4235a2825ad8660ff6982adbb97
Poppynick <nmadison98@gmail.com>
2025-10-29 10:28:24 -0400

    added functionality by passing a second argument for the book path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count
 from stats import char_count
 from stats import sort_char
+import sys
 
 def get_book_text(path):
     with open(path) as f:
@@ -10,7 +11,14 @@ def get_book_text(path):
 
 
 def main():
-    frank = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) == 2:
+        book_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+
+
+    frank = get_book_text(book_path)
     text = word_count(frank)
     count = len(text)
     char_list = []
@@ -24,7 +32,7 @@ def main():
     char_list.sort(reverse=True, key=sort_char)
     
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
     print("--------- Character Count -------")
